6542e29c8449d2b2a1620cb5529e42e8fd2d4650
Anush-Kas <anushkasparova@gmail.com>
2022-07-15 09:31:54 +0300

    Внесла изменения в игру

--- pygamexo.py
+++ pygamexo.py
@@ -27,7 +27,7 @@ def igrok(player_key):
             else:
                 print("Эта клетка уже занята!")
         else:
-            print("Error")
+            print("Я сломался, больше не играю!")
 
 def check_win(table):
     win_position = ((0, 1, 2), (3, 4, 5),(6, 7, 8),(0, 3, 6),(1, 4, 7),(2, 5, 8),(0, 4, 8),(2, 4, 6))
